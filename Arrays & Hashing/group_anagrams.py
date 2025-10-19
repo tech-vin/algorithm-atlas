0ab0366773ca24c35e9591e2811931bfe24c3543
tech-vin <tech.vineetsingh@gmail.com>
2025-10-20 03:35:24 +0530

    group_anagreams.py uploaded

---
+++ Arrays & Hashing/group_anagrams.py
@@ -0,0 +1,25 @@
+# https://neetcode.io/problems/anagram-groups?list=neetcode150
+
+from collections import defaultdict
+
+def group_anagrams(strs: list) -> list:
+    groups = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - ord('a')] += 1
+        groups[tuple(count)].append(s)
+    return list(groups.values())
+
+testcases = [
+    ["act","pots","tops","cat","stop","hat"],   # [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+    ["x"],                                      # [["x"]]
+    [""]                                        # [[""]]
+]
+
+print([group_anagrams(x) for x in testcases])
+
+
+
+
